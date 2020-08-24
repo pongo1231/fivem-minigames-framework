2c34060f89e1464538e15db737931147b3a9c799
pongo1231 <pongo1999712@gmail.com>
2020-08-24 15:32:24 +0200

    Add bin folder

---
+++ bin/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version "cerulean"
+games { "gta5" }
+
+author "pongo1231"
+description "sick af gamemodes"
+version "0.1"
+
+client_script "GamemodesClient.net.dll"
+
+server_script "GamemodesServer.net.dll"
+
+shared_script "GamemodesShared.net.dll"
